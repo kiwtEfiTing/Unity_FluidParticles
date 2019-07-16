0f8034fabe983afa4232a9ae4fcf730b008bf3a7
matamata <>
2019-07-16 20:12:34 +0800

    initial

---
+++ Assets/MyScript/FollowTargetPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTargetPosition : MonoBehaviour {
+
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.localPosition = new Vector3(target.transform.localPosition.x,
+                                              target.transform.localPosition.y,
+                                              target.transform.localPosition.z);
+
+	}
+}
